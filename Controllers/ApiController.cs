ca3f7c25ed7fff48d44e114140df9332e4d0d92d
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2021-07-05 18:16:57 +0100

    Initial Commit - Base ASP Site

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMPackageManager.Website.Controllers {
+    [Route("api")]
+    public class ApiController : Controller {
+        public IActionResult Index() {
+            return Ok("TODO: API Routes");
+        }
+    }
+}
